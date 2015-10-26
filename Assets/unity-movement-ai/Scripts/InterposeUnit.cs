131df20246c52699bab22c16f54e453249c2211f
Anton Pantev <pantevanton@gmail.com>
2015-10-26 14:31:52 -0700

    Making interpose generic.

--- Assets/unity-movement-ai/Scripts/InterposeUnit.cs
+++ Assets/unity-movement-ai/Scripts/InterposeUnit.cs
@@ -3,21 +3,27 @@ using System.Collections;
 
 public class InterposeUnit : MonoBehaviour {
 
-    public Rigidbody target1;
-    public Rigidbody target2;
+    public GameObject target1;
+    public GameObject target2;
+
+    private GenericRigidbody targetRigidbody1;
+    private GenericRigidbody targetRigidbody2;
 
     private SteeringBasics steeringBasics;
 
     // Use this for initialization
     void Start()
     {
+        targetRigidbody1 = SteeringBasics.getGenericRigidbody(target1);
+        targetRigidbody2 = SteeringBasics.getGenericRigidbody(target2);
+
         steeringBasics = GetComponent<SteeringBasics>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 accel = steeringBasics.interpose(target1, target2);
+        Vector3 accel = steeringBasics.interpose(targetRigidbody1, targetRigidbody2);
 
         steeringBasics.steer(accel);
         steeringBasics.lookWhereYoureGoing();
