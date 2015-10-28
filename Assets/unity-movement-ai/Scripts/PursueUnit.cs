3483f4e7825de221a74b5a133f20b324cc73215e
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2015-10-28 00:38:08 -0700

    adding a 2d pursue example scene

--- Assets/unity-movement-ai/Scripts/PursueUnit.cs
+++ Assets/unity-movement-ai/Scripts/PursueUnit.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class PursueUnit : MonoBehaviour {
 
-    public Rigidbody target;
+    public GameObject target;
+    private GenericRigidbody targetRigidbody;
 
     private SteeringBasics steeringBasics;
     private Pursue pursue;
 
     // Use this for initialization
     void Start () {
+        targetRigidbody = SteeringBasics.getGenericRigidbody(target);
+
         steeringBasics = GetComponent<SteeringBasics>();
         pursue = GetComponent<Pursue>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 accel = pursue.getSteering(target);
+        Vector3 accel = pursue.getSteering(targetRigidbody);
 
         steeringBasics.steer(accel);
         steeringBasics.lookWhereYoureGoing();
