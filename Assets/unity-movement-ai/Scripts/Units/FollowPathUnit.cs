47ef2fe51b2736d08935d4d56b58467a84774da1
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2015-11-08 21:05:35 -0800

    very in progress

--- Assets/unity-movement-ai/Scripts/Units/FollowPathUnit.cs
+++ Assets/unity-movement-ai/Scripts/Units/FollowPathUnit.cs
@@ -12,21 +12,34 @@ public class FollowPathUnit : MonoBehaviour {
     private SteeringBasics steeringBasics;
     private FollowPath followPath;
 
+    private float startTime;
+    private int count = 0;
+    private Vector3 lastPos;
+    private float distTraveled = 0;
+
     // Use this for initialization
     void Start () {
         path.calcDistances();
 
         steeringBasics = GetComponent<SteeringBasics>();
         followPath = GetComponent<FollowPath>();
+
+        startTime = Time.time;
+        lastPos = transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
         path.draw();
 
+        distTraveled += Vector3.Distance(lastPos, transform.position);
+        lastPos = transform.position;
         if (reversePath && isAtEndOfPath())
         {
             path.reversePath();
+            Debug.Log(gameObject.name + " " + (Time.time - startTime) + " " + distTraveled + " " + (distTraveled / (Time.time - startTime)) + " " + count);
+            startTime = Time.time;
+            count++;
         }
 
         Vector3 accel = followPath.getSteering(path, pathLoop);
