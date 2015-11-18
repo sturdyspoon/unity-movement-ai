3a8a460be5b2019e992022b18c73915a7abd7ff6
Anton <pantevanton@gmail.com>
2015-11-18 12:44:10 -0800

    Making a complex 3D scene.

---
+++ Assets/Editor/RandomizeTerrainEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(RandomizeTerrain))]
+public class RandomizeTerrainEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RandomizeTerrain myScript = (RandomizeTerrain) target;
+
+
+
+        if (createCenteredButton("Randomize Terrain"))
+        {
+            myScript.randomize();
+        }
+    }
+
+    public bool createCenteredButton(string text)
+    {
+        GUIContent content = new GUIContent(text);
+
+        Rect rect = GUILayoutUtility.GetRect(content, GUI.skin.button, GUILayout.ExpandWidth(false));
+        rect.position = new Vector2((Screen.width - rect.size.x) / 2, rect.position.y);
+
+        return GUI.Button(rect, content, GUI.skin.button);
+    }
+}
--- Assets/unity-movement-ai/Scripts/RandomizeTerrain.cs
+++ Assets/unity-movement-ai/Scripts/RandomizeTerrain.cs
@@ -7,9 +7,24 @@ public class RandomizeTerrain : MonoBehaviour {
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void randomize()
+    {
+        Debug.Log("YOOO");
+    }
+
+    public void generateHeights(Terrain terrain, float tileSize)
+    {
+        float[,] heights = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
+
+        for (int i = 0; i < terrain.terrainData.heightmapWidth; i++)
+        {
+            for (int k = 0; k < terrain.terrainData.heightmapHeight; k++)
+            {
+                heights[i, k] = Mathf.PerlinNoise(((float)i / (float)terrain.terrainData.heightmapWidth) * tileSize, ((float)k / (float)terrain.terrainData.heightmapHeight) * tileSize) / 10.0f;
+            }
+        }
+
+        terrain.terrainData.SetHeights(0, 0, heights);
+    }
 }
