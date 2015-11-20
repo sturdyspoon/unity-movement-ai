694910af6b8f0bce6b1999b3dbef82511f740ca8
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2015-11-19 18:42:59 -0800

    randomizing the ground

--- Assets/unity-movement-ai/Scripts/RandomizeTerrain.cs
+++ Assets/unity-movement-ai/Scripts/RandomizeTerrain.cs
@@ -3,14 +3,11 @@ using System.Collections;
 
 public class RandomizeTerrain : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public float tiling = 10.0f;
 
     public void randomize()
     {
-        Debug.Log("YOOO");
+        generateHeights(GetComponent<Terrain>(), tiling);
     }
 
     public void generateHeights(Terrain terrain, float tileSize)
