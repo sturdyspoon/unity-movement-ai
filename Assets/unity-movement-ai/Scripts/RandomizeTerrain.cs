166f8fd66ef2e1ecd3cb9c7c78b5176e4515cf10
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2015-11-23 14:31:04 -0800

    Made Randomize Terrain really be random. Added a simple First Person Camera script to the camera.

--- Assets/unity-movement-ai/Scripts/RandomizeTerrain.cs
+++ Assets/unity-movement-ai/Scripts/RandomizeTerrain.cs
@@ -3,22 +3,24 @@ using System.Collections;
 
 public class RandomizeTerrain : MonoBehaviour {
 
-    public float tiling = 10.0f;
+    public float perlinScale = 10.0f;
 
     public void randomize()
     {
-        generateHeights(GetComponent<Terrain>(), tiling);
+        generateHeights(GetComponent<Terrain>(), perlinScale);
     }
 
-    public void generateHeights(Terrain terrain, float tileSize)
+    public void generateHeights(Terrain terrain, float perlinScale)
     {
+        PerlinHelper ph = new PerlinHelper(terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight, perlinScale);
+
         float[,] heights = new float[terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight];
 
         for (int i = 0; i < terrain.terrainData.heightmapWidth; i++)
         {
             for (int k = 0; k < terrain.terrainData.heightmapHeight; k++)
             {
-                heights[i, k] = Mathf.PerlinNoise(((float)i / (float)terrain.terrainData.heightmapWidth) * tileSize, ((float)k / (float)terrain.terrainData.heightmapHeight) * tileSize) / 10.0f;
+                heights[i, k] = ph[i, k];
             }
         }
 
