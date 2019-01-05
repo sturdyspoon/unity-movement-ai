b7b35622270e633561a19a0d4d6b6b74f76b9298
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2019-01-05 18:32:47 -0500

    Moved around files.

--- Assets/UnityMovementAI/Editor/RandomizeTerrainEditor.cs
+++ Assets/UnityMovementAI/Editor/RandomizeTerrainEditor.cs
@@ -24,7 +24,6 @@ namespace UnityMovementAI
             GUIContent content = new GUIContent(text);
 
             Rect rect = GUILayoutUtility.GetRect(content, GUI.skin.button, GUILayout.ExpandWidth(false));
-            rect.position = new Vector2((Screen.width - rect.size.x) / 2, rect.position.y);
 
             return GUI.Button(rect, content, GUI.skin.button);
         }
